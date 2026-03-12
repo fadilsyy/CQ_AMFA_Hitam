3c825aba45238a5c6446bc5f6e4077398b3c8740
Fadil Asyy <fadilasytar@gmail.com>
2026-03-12 22:16:20 +0800

    method button connect

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -23,6 +23,24 @@ namespace PraktikumADO
                 "Data Source=SHIROKO\\FADILSYY;Initial Catalog=DBAkademikADO;Integrated Security=True"
             );
 
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                MessageBox.Show("Koneksi ke database berhasil");
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
     }
 }
