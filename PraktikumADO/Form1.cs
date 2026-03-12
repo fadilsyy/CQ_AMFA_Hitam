5d4ae39b89020490e255d7bdb13686b5f33d0993
Fadil Asyy <fadilasytar@gmail.com>
2026-03-12 22:11:41 +0800

    membuat namespace baru

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -1,3 +1,11 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace PraktikumADO
+{
+
+}
